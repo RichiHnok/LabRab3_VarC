a67f095cd7070047b9e4190494ebf52617c1c78f
Richi Hnok <grenkavoit@gmail.com>
2022-11-12 03:02:09 +0300

    сделал первую версию MyGornerTableCellRenderer

---
+++ MyGornerTableCellRenderer.java
@@ -0,0 +1,49 @@
+import java.awt.Color;
+import java.awt.Component;
+import javax.swing.JTable;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.FlowLayout;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+import javax.swing.table.TableCellRenderer;
+
+public class MyGornerTableCellRenderer implements TableCellRenderer {
+    
+    private String needle = null;
+
+    private JPanel panel = new JPanel();
+    private JLabel label = new JLabel();
+
+    private DecimalFormat formatter = (DecimalFormat)NumberFormat.getInstance();
+
+
+    public MyGornerTableCellRenderer(){
+        panel.add(label);
+        panel.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+        formatter.setMaximumFractionDigits(5);
+        formatter.setGroupingUsed(false);
+        DecimalFormatSymbols dottedDouble = formatter.getDecimalFormatSymbols();
+        dottedDouble.setDecimalSeparator('.');
+        formatter.setDecimalFormatSymbols(dottedDouble);
+    }    
+    
+    public void setNeedle(String needle){
+        this.needle = needle;
+    }    
+    
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column){
+        String formattedDouble = formatter.format(value);
+
+        label.setText(formattedDouble);
+        if(column == 1 && needle != null && needle.equals(formattedDouble)){
+            panel.setBackground(Color.RED);
+        }else{
+            panel.setBackground(Color.WHITE);
+        }
+        return panel;
+    }
+}
